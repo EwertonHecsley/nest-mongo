0c7c22c837d7ad97ccab72c9ee6c87093adb4f6d
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-08-04 19:30:50 -0300

    add hash into password

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './repository/user.repository';
 import { PrismaService } from 'src/infra/orm/prisma.service';
+import { HashService } from 'src/infra/hash/hash.service';
 
 @Module({
-  providers: [UserService, UserRepository, PrismaService],
+  providers: [UserService, UserRepository, PrismaService, HashService],
   controllers: [UserController]
 })
 export class UserModule { }
--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,10 +1,14 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserRepository } from './repository/user.repository';
 import { UserDto } from './dto/user.dto';
+import { HashService } from 'src/infra/hash/hash.service';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly userRepository: UserRepository) { }
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly hashService: HashService
+    ) { }
 
     async findAll(): Promise<UserDto[]> {
         return await this.userRepository.findAll();
@@ -20,6 +24,8 @@ export class UserService {
     async create(data: UserDto): Promise<UserDto> {
         const userExist = await this.userRepository.findByEmail(data.email);
         if (userExist) throw new HttpException("Email já cadastrado.", HttpStatus.BAD_REQUEST);
+
+        data.password = await this.hashService.hashPassword(data.password);
 
         return await this.userRepository.create(data);
     }
